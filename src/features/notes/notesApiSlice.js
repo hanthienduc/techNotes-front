005ec7efecf85d6e1f8ebfc3734b6e474c288ca6
hanthienduc <hanthienduc.96@gmail.com>
2022-10-27 21:08:57 +0900

    add CRUD api request for user and note feature, also add initial UI component for edit and add new note and user form

--- src/features/notes/notesApiSlice.js
+++ src/features/notes/notesApiSlice.js
@@ -33,10 +33,45 @@ export const notesApiSlice = apiSlice.injectEndpoints({
                 } else return [{ type: 'User', id: 'LIST' }]
             },
         }),
+        addNewNote: builder.mutation({
+            query: (initialNote) => ({
+                url: '/notes',
+                method: 'POST',
+                body: {
+                    ...initialNote,
+                },
+            }),
+            invalidatesTags: [{ type: 'Note', id: 'LIST' }],
+        }),
+        updateNote: builder.mutation({
+            query: (initialNote) => ({
+                url: '/notes',
+                method: 'PATCH',
+                body: [...initialNote],
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'Note', id: arg.id },
+            ],
+        }),
+        deleteNote: builder.mutation({
+            query: ({ id }) => ({
+                url: `/notes`,
+                method: 'DELETE',
+                body: { id },
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'Note', id: arg.id },
+            ],
+        }),
     }),
 })
 
-export const { useGetNotesQuery } = notesApiSlice
+export const {
+    useGetNotesQuery,
+    useAddNewNoteMutation,
+    useUpdateNoteMutation,
+    useDeleteNoteMutation,
+} = notesApiSlice
 
 // returns the query result object
 export const selectUserResult = notesApiSlice.endpoints.getnotes.select()
