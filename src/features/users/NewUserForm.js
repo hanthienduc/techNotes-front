5680440bedeb4d31d053f658c3b1399697421390
hanthienduc <hanthienduc.96@gmail.com>
2022-10-28 21:08:53 +0900

    finish add userform form

--- src/features/users/NewUserForm.js
+++ src/features/users/NewUserForm.js
@@ -17,8 +17,124 @@ const NewUserForm = () => {
     const [validUsername, setValidUsername] = useState(false)
     const [password, setPassword] = useState('')
     const [validPassword, setValidPassword] = useState(false)
-    
-    return <div>NewUserForm</div>
+    const [roles, setRoles] = useState(['Employee'])
+
+    useEffect(() => {
+        setValidUsername(USER_REGEX.test(username))
+    }, [username])
+
+    useEffect(() => {
+        setValidPassword(PWD_REGEX.test(password))
+    }, [password])
+
+    useEffect(() => {
+        if (isSuccess) {
+            setUsername('')
+            setPassword('')
+            setRoles([])
+            navigate('/dash/users')
+        }
+    }, [isSuccess, navigate])
+
+    const onUserNameChanged = (e) => setUsername(e.target.value)
+    const onPasswordChanged = (e) => setPassword(e.target.value)
+
+    const onRolesChanged = (e) => {
+        const values = Array.from(
+            e.target.selectedOptions, // HTMLCollection
+            (option) => option.value
+        )
+        setRoles(values)
+    }
+
+    const canSave =
+        [roles.length, validUsername, validPassword].every(Boolean) &&
+        !isLoading
+
+    const onSaveUserClicked = async (e) => {
+        e.preventDefault()
+        if (canSave) {
+            await addNewUser({ username, password, roles })
+        }
+    }
+
+    const options = Object.values(ROLES).map((role) => {
+        return (
+            <option key={role} value={role}>
+                {role}
+            </option>
+        )
+    })
+
+    const errClass = isError ? 'errmsg' : 'offscreen'
+    const validUserClass = !validUsername ? 'form__input--incomplete' : ''
+    const validPwdClass = !validPassword ? 'form__input--incomplete' : ''
+    const validRolesClass = !Boolean(roles.length)
+        ? 'form__input--incomplete'
+        : ''
+
+    const content = (
+        <>
+            <p className={errClass}>{error?.data?.message}</p>
+
+            <form className="form" onSubmit={onSaveUserClicked}>
+                <div className="form__title-row">
+                    <h2>New User</h2>
+                    <div className="form__action-buttons">
+                        <button
+                            className="icon-button"
+                            title="save"
+                            disabled={!canSave}
+                        >
+                            <FontAwesomeIcon icon={faSave} />
+                        </button>
+                    </div>
+                </div>
+                <label className="form__label" htmlFor="username">
+                    Username: <span className="nowrap">[3-20 letters]</span>
+                </label>
+                <input
+                    className={`form__input ${validUserClass}`}
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete="off"
+                    value={username}
+                    onChange={onUserNameChanged}
+                />
+
+                <label className="form__label" htmlFor="password">
+                    Password:{' '}
+                    <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+                </label>
+                <input
+                    className={`form__input ${validPwdClass}`}
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={onPasswordChanged}
+                />
+
+                <label className="form__label" htmlFor="roles">
+                    ASSIGNED ROLES:
+                </label>
+                <select
+                    id="roles"
+                    name="roles"
+                    className={`form__select ${validRolesClass}`}
+                    multiple={true}
+                    size="3"
+                    value={roles}
+                    onChange={onRolesChanged}
+                >
+                    {options}
+                </select>
+            </form>
+        </>
+    )
+
+    return content
 }
 
 export default NewUserForm
